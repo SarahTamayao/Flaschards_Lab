0cbe82e8271cb16fd96a3ba4981f80d64b253785
Nelson Dufitimana <nelsondft10@gmail.com>
2022-02-26 18:09:43 -0500

    Lab 1

--- Lab1/ViewController.swift
+++ Lab1/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var FrontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        FrontLabel.isHidden = true
+    }
+    
 }
 
