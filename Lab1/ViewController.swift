1b3dabaf234efde8d127069820953b6966fd902f
Nelson Dufitimana <nelsondft10@gmail.com>
2022-02-26 14:02:57 -0500

    Initial Commit

---
+++ Lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1
+//
+//  Created by Nelson Dufitimana on 26/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
